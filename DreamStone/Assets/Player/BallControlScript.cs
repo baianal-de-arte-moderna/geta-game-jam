1ee603ab8ffc13f07e6b8111d1b1725f6de214bf
Piramba <avpaganini@gmail.com>
2019-07-06 00:53:25 -0300

    Player Ball

---
+++ DreamStone/Assets/Player/BallControlScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControlScript : MonoBehaviour
+{
+    public string navigationPlaneLayer;
+    public GameObject player;
+
+    Transform playerInstance;
+    int layerMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInstance = Instantiate(player).transform;
+        playerInstance.SetParent(transform);
+        layerMask = LayerMask.GetMask(navigationPlaneLayer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 100.0f, layerMask))
+        {
+            transform.position = Vector3.Lerp(
+                transform.position,
+                hit.point,
+                0.2f
+            );
+        }
+    }
+}
---
+++ DreamStone/Assets/Player/BallWobbleScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallWobbleScript : MonoBehaviour
+{
+    public float wobbleIntensity;
+    void Update()
+    {
+        transform.localPosition = Vector3.Lerp(
+            transform.localPosition,
+            Random.insideUnitSphere,
+            wobbleIntensity / 10f
+            );
+    }
+}
