1f958c3bbbf1b940bf6ac5fd7e75b10c5e4db360
Piramba <avpaganini@gmail.com>
2019-07-06 14:24:39 -0300

    Thoughts and prayers

--- DreamStone/Assets/Scripts/BedBehaviourScript.cs
+++ DreamStone/Assets/Scripts/BedBehaviourScript.cs
@@ -1,20 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class BedBehaviourScript : MonoBehaviour
 {
     public string sceneName;
+    public Animator thoughts;
 
     public void Select()
     {
         transform.localScale *= 1.2f;
+        thoughts.gameObject.SetActive(true);
+        thoughts.SetTrigger("FadeIn");
     }
 
     public void Unselect()
     {
         transform.localScale /= 1.2f;
+        thoughts.SetTrigger("FadeOut");
     }
 
     public void Interact()
---
+++ DreamStone/Assets/Scripts/BillboardScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Camera.main.transform);
+        transform.Rotate(Vector3.up, 180);
+    }
+}
