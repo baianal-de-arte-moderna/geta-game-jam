bc91023ed46be266d94007486eff0669ac8bcc79
Piramba <avpaganini@gmail.com>
2019-07-06 02:19:47 -0300

    Dream Room models

---
+++ DreamStone/Assets/Scripts/BedInteractionScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BedInteractionScript : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bed"))
+        {
+            var bed = other.GetComponent<BedBehaviourScript>();
+            bed?.Select();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Bed"))
+        {
+            var bed = other.GetComponent<BedBehaviourScript>();
+            bed?.Unselect();
+        }
+    }
+}
