bf8df11968d440163a849eeb8d1d69f337f3077a
Piramba <avpaganini@gmail.com>
2019-07-06 15:12:06 -0300

    Scene Transition panel

---
+++ DreamStone/Assets/SceneTransitionAnimation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneTransitionAnimation : MonoBehaviour
+{
+    Color targetColor;
+    public Image panel;
+    public float transitionSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetColor = Color.clear;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        panel.color = Color.Lerp(
+            panel.color,
+            targetColor,
+            transitionSpeed
+        );
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        targetColor = Color.white;
+        StartCoroutine(LoadScene(sceneName));
+    }
+
+    IEnumerator LoadScene(string name)
+    {
+        yield return new WaitForSeconds(0.1f / transitionSpeed);
+        SceneManager.LoadScene(name);
+    }
+}
--- DreamStone/Assets/Scripts/BedBehaviourScript.cs
+++ DreamStone/Assets/Scripts/BedBehaviourScript.cs
@@ -1,26 +1,24 @@
 ﻿using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class BedBehaviourScript : MonoBehaviour
 {
     public string sceneName;
     public Animator thoughts;
+    public SceneTransitionAnimation sceneTransition;
 
     public void Select()
     {
-        transform.localScale *= 1.2f;
         thoughts.gameObject.SetActive(true);
         thoughts.SetTrigger("FadeIn");
     }
 
     public void Unselect()
     {
-        transform.localScale /= 1.2f;
         thoughts.SetTrigger("FadeOut");
     }
 
     public void Interact()
     {
-        SceneManager.LoadScene(sceneName);
+        sceneTransition.ChangeScene(sceneName);
     }
 }
