bf5db0df3b42de1fca9b817ba463ceac0d3302ee
Vinicius Uzeda <vruzeda@gmail.com>
2019-07-07 18:27:00 -0300

    Improve character beahviour

--- DreamStone/Assets/Scripts/Behaviours/WalkForwardBehaviour.cs
+++ DreamStone/Assets/Scripts/Behaviours/WalkForwardBehaviour.cs
@@ -5,8 +5,17 @@ public class WalkForwardBehaviour : BaseBehaviour {
     [SerializeField]
     private float walkForce;
 
+    private enum State {
+        Idle,
+        Walking,
+        Dead
+    }
+
     private new Rigidbody rigidbody;
     private Vector3 startPosition;
+    private State state;
+    private Vector3 force;
+    private float accumulatedTime;
 
     private void Awake() {
         rigidbody = GetComponent<Rigidbody>();
@@ -22,14 +31,40 @@ public class WalkForwardBehaviour : BaseBehaviour {
     }
 
     public override void Iterate() {
-        if (rigidbody.velocity == Vector3.zero) {
-            rigidbody.AddForce(transform.forward * walkForce);
+        if (state == State.Dead) {
+            accumulatedTime += Time.deltaTime;
+            if (accumulatedTime > 5f) {
+                SetState(State.Idle);
+            }
         }
     }
 
-    private void OnTriggerExit(Collider other) {
-        if (other.CompareTag("Respawn")) {
-            transform.position = startPosition;
+    private void SetState(State newState) {
+        Debug.Log($"Change state from {state} to {newState}");
+
+        state = newState;
+        switch (state) {
+            case State.Idle:
+                transform.position = startPosition;
+                break;
+            case State.Walking:
+                rigidbody.AddForce(-force);
+                force = transform.forward * walkForce;
+                rigidbody.AddForce(force);
+                break;
+            case State.Dead:
+                rigidbody.AddForce(-force);
+                force = Vector3.zero;
+                accumulatedTime = 0f;
+                break;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.collider.CompareTag("Cliff") && state == State.Idle) {
+            SetState(State.Walking);
+        } else if (collision.collider.CompareTag("Floor") && state == State.Walking) {
+            SetState(State.Dead);
         }
     }
 }
