12d312fa916c7bd25c95c81606ceecb6db5eb9ab
Piramba <avpaganini@gmail.com>
2019-07-07 19:17:43 -0300

    Real world scene

---
+++ DreamStone/Assets/Scripts/OrphanageCameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrphanageCameraScript : MonoBehaviour
+{
+    public float minAngle = 3f;
+    public float maxAngle = 30f;
+    public float speed = 10f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.mousePosition.x >= Screen.width * 0.9 && transform.localRotation.eulerAngles.y < maxAngle)
+        {
+            transform.Rotate(Vector3.up * speed * (Input.mousePosition.x / Screen.width), Space.World);
+        }
+        if (Input.mousePosition.x <= Screen.width * 0.1 && transform.localRotation.eulerAngles.y > minAngle)
+        {
+            transform.Rotate(Vector3.down * speed * ((Screen.width - Input.mousePosition.x) / Screen.width), Space.World);
+        }
+    }
+}
---
+++ DreamStone/Assets/Voxel/Character/CharacterAnimatorControlScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimatorControlScript : MonoBehaviour
+{
+    public Animator animator;
+    public Rigidbody rigid;
+
+    // Update is called once per frame
+    void Update() 
+    {
+        animator.SetFloat("Velocity", rigid.velocity.magnitude);
+    }
+}
