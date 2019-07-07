301c21005312cd61b239cab5fc00a3eb1757739f
Vinicius Uzeda <vruzeda@gmail.com>
2019-07-07 16:20:23 -0300

    Improve character behaviour in cliff dream scene

--- DreamStone/Assets/Scripts/Behaviours/HaltOnObjectBehaviour.cs
+++ DreamStone/Assets/Scripts/Behaviours/HaltOnObjectBehaviour.cs
@@ -1,31 +1,38 @@
 /* vim: set ts=4 sts=4 sw=4 expandtab: */
 using UnityEngine;
 
-public class HaltOnObjectBehaviour : BaseBehaviour
-{
+public class HaltOnObjectBehaviour : BaseBehaviour {
+
     [SerializeField]
     private string haltedOnObjectTag;
 
     private new Rigidbody rigidbody;
+    private Vector3 startVelocity;
 
-    protected void Awake()
-    {
+    protected void Awake() {
         rigidbody = GetComponent<Rigidbody>();
     }
 
-    public override bool InterruptChain()
-    {
+    public override bool IsActive() {
+        bool isActive = GameObject.FindWithTag(haltedOnObjectTag) != null;
+
+        if (!isActive && startVelocity != Vector3.zero) {
+            rigidbody.velocity = startVelocity;
+            startVelocity = Vector3.zero;
+        }
+
+        return isActive;
+    }
+
+    public override bool InterruptChain() {
         return true;
     }
 
-    public override bool IsActive()
-    {
-        return GameObject.FindWithTag(haltedOnObjectTag) != null;
-    }
-
-    public override void Iterate()
-    {
-        rigidbody.velocity = Vector3.Dot(rigidbody.velocity, rigidbody.transform.up) * rigidbody.transform.up;
+    public override void Iterate() {
+        if (rigidbody.velocity != Vector3.zero) {
+            startVelocity = rigidbody.velocity;
+            rigidbody.velocity = Vector3.zero;
+        }
     }
 }
 
