e0d4dae21791ac0047b9c301a75b4f86e27bdc1f
Piramba <avpaganini@gmail.com>
2019-07-08 03:15:12 -0300

    Main menu

---
+++ DreamStone/Assets/LogoWobbleScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoWobbleScript : MonoBehaviour
+{
+    public float intensity;
+    float rand;
+    Vector3 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = Random.insideUnitSphere;
+        rand = Random.Range(2f, 4f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rand -= Time.deltaTime;
+        if (rand < 0)
+        {
+            rand = Random.Range(2f, 4f);
+            direction = Random.insideUnitSphere;
+        }
+        transform.Rotate(direction * intensity * Time.deltaTime);
+        transform.Translate(direction * intensity / 100f * Time.deltaTime);
+    }
+}
